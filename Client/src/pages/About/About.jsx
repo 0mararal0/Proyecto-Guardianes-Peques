e96fc342a714d5aa13800d208c2d37abb3e03347
Miguel Angel <migueel26@gmail.com>
2024-09-30 15:05:52 +0200

    pagina about, contact, legal notice

--- Client/src/pages/About/About.jsx
+++ Client/src/pages/About/About.jsx
@@ -5,22 +5,22 @@ export const About = () => {
   return (
     <section>
       <div className="about-div">
-        <h1>Acerca de nosotros</h1>
+        <h1 className="titulo">Acerca de nosotros</h1>
         <div>
-          <h3>Conectamos familias con cuidadores.</h3>
-          <p>
+          <h3 className="titulo">Conectamos familias con cuidadores.</h3>
+          <p className="texto">
           Guardianes & Peques es una plataforma creada con el objetivo de 
           ofrecer atención de calidad a los niños, entendiendo que este cuidado 
           es esencial para el bienestar familiar y el desarrollo saludable de los pequeños.
           </p>
-          <p>Nuestra misión en Guardianes & Peques es facilitar a las familias la búsqueda 
+          <p className="texto">Nuestra misión en Guardianes & Peques es facilitar a las familias la búsqueda 
             de cuidadores especializados que se adapten a sus necesidades, al tiempo que 
             brindamos oportunidades laborales a profesionales en el ámbito del cuidado infantil.
           </p>
         </div>
         <div>
-          <h3>¿Qué ofrecemos?</h3>
-          <ul>
+          <h3 className="titulo">¿Qué ofrecemos?</h3>
+          <ul className="texto">
           <li>
           Para las familias, proporcionamos una solución accesible y completa que simplifica 
           la búsqueda de niñeras, educadores, cuidadores con experiencia en necesidades especiales, 
