ffb97c5d42feb148c5ec89d11821c94f21323fc5
alberto marcos <amarcosarranz@gmail.com>
2024-10-03 00:37:27 +0200

    borrar guardian, modificar guardian, mostrar guardianes

--- Client/src/pages/Guardians/Guardians.jsx
+++ Client/src/pages/Guardians/Guardians.jsx
@@ -1,30 +1,76 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
-import { CardGuardian } from "../../components/CardGuardian/CardGuardian";
+import { useNavigate } from "react-router-dom";
 
 export const Guardians = () => {
-  const [dataGuardian, setDataGuardian] = useState();
+  const [dataGuardian, setDataGuardian] = useState([]);
+  const [deleted, setDeleted] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get("http://localhost:4000/guardian")
       .then((res) => {
         setDataGuardian(res.data);
+        setDeleted(false);
       })
-
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [deleted]);
+  console.log(deleted);
   console.log(dataGuardian);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:4000/guardian/${id}`)
+      .then((res) => {
+        console.log("datos borrados correctamente", res.data);
+        setDeleted(true);
+        setDataGuardian("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  if (dataGuardian.length === 0) {
+    return <h3>...un momento por favor</h3>;
+  }
 
   return (
     <>
       <div>
-        {dataGuardian?.map((elem) => {
-          return <CardGuardian key={elem.id} id={elem.id} />;
-        })}
+        {dataGuardian.length !== 0 &&
+          dataGuardian?.map((elem) => {
+            return (
+              <div key={elem.id}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={elem?.foto} />
+                  <Card.Body>
+                    <Card.Title>
+                      {elem?.nombre} {elem?.apellidos}
+                      {}
+                    </Card.Title>
+                    <Card.Text>{elem?.comentario}</Card.Text>
+                  </Card.Body>
+                  <Button
+                    variant="primary"
+                    onClick={() => handleDelete(elem.id)}
+                  >
+                    Eliminar
+                  </Button>
+                  <Button
+                    variant="primary"
+                    onClick={() => navigate(`/editGuardian/${elem.id}`)}
+                  >
+                    Editar
+                  </Button>
+                </Card>
+              </div>
+            );
+          })}
       </div>
     </>
   );
